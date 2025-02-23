5e6dc06612e28e1a51ae802eb605e6ee425e905b
Daria Kolpakova <daka.kolp01@gmail.com>
2025-02-23 19:35:42 +0200

    initial commit

---
+++ NewsClient/NewsClient/NewsClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsClientApp.swift
+//  NewsClient
+//
+//  Created by Daria Kolpakova on 23.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
